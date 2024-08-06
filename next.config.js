b2c603392847c5de8e8982385fc8d68260da8fda
Philippe Côté <phil@bleuet.design>
2024-08-06 12:52:46 -0400

    Trial to redirect old URLs

--- next.config.js
+++ next.config.js
@@ -7,4 +7,41 @@ const nextConfig = {
   reactStrictMode: false,
 };
 
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://gaianinja.webflow.io/',
+        permanent: true,
+      },
+      {
+        source: '/picobo',
+        destination: 'https://gaianinja.webflow.io/picobo',
+        permanent: true,
+      },
+      {
+        source: '/picobo-ecole',
+        destination: 'https://gaianinja.webflow.io/picobo-ecole',
+        permanent: true,
+      },
+      {
+        source: '/a-propos',
+        destination: 'https://gaianinja.webflow.io/a-propos',
+        permanent: true,
+      },
+      {
+        source: '/support',
+        destination: 'https://gaianinja.webflow.io/support',
+        permanent: true,
+      },
+      {
+        source: '/picobo/prof',
+        destination: 'https://gaianinja.webflow.io/picobo/prof',
+        permanent: true,
+      },
+    ]
+  },
+}
+
 module.exports = nextConfig;
